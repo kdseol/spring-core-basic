a41a1a56f06fbf18595f6f652d9f5483d9ce41c7
seolkd <seolkd.e@gmail.com>
2022-03-13 09:57:50 +0900

    스프링 4.3이후부터는 생성자가 하나일 경우는 @Autowired를 생략 가능하다

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,6 +1,5 @@
 package hello.core.member;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +7,7 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Autowired // 생성자에 붙여주면 스프링이 자동으로 의존관계 주입을 해 준다. (== ac.getBean(MemberRepository.class))
+    //@Autowired // 생성자에 붙여주면 스프링이 자동으로 의존관계 주입을 해 준다. (== ac.getBean(MemberRepository.class))
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -28,3 +27,29 @@ public class MemberServiceImpl implements MemberService {
         return memberRepository;
     }
 }
+
+/*
+ * 생성자 주입(Field Injection)
+    private final MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+ * 필드 주입 (Construction Injection)
+    @Autowired
+    private MemberRepository memberRepository;
+
+ * 수정자 주입(Setter Injection)
+   private MemberRepository memberRepository;
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+  * As of Spring 4.3, classes with a single constructor can omit the @Autowired annotation
+
+  * 생성자 주입을 권장하는 이유
+   1. 순환참조 방지 / 2. 불변성 / 3. 테스트코드 작성
+
+*/
